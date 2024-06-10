1e22c7da6d7b2dde692927ed619834e560497b71
류솔아 <mav7937@naver.com>
2024-06-10 16:01:56 +0900

    슬라이드 폼은 완성, loop에서 오류, 아직 해결하지 못함

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(function () {
     loopedSlides: 6,
     direction: 'vertical',
 
-    autoplay: { delay: 5000 },
+    // autoplay: { delay: 5000 },
 
     // centeredSlides: true,
 
@@ -15,8 +15,27 @@ $(function () {
     spaceBetween: 20,
     // Navigation arrows
     navigation: {
-      nextEl: '.btn-up',
-      prevEl: '.btn-down',
+      nextEl: '.btn-down',
+      prevEl: '.btn-up',
     },
   });
+  const projectImg = new Swiper('.project-img', {
+    speed: 500,
+    loop: true,
+    loopedSlides: 6,
+    direction: 'vertical',
+
+    // autoplay: { delay: 5000 },
+
+    // centeredSlides: true,
+
+    // slideToClickedSlide: true,
+
+    slidesPerView: 1,
+    spaceBetween: 20,
+    // Navigation arrows
+  });
+
+  projectImg.controller.control = projectText;
+  projectText.controller.control = projectImg;
 });
